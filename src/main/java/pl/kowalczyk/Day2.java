3513181f8e058477e24b81304045b94bbfd9413e
Marcin Kowalczyk <kowalczyk.m@int.pl>
2020-02-05 00:04:44 +0100

    code works. TO DO: clean code

--- src/main/java/pl/kowalczyk/Day2.java
+++ src/main/java/pl/kowalczyk/Day2.java
@@ -31,33 +31,36 @@ public class Day2 {
 
     private static List<Integer> modifiedList(List<Integer> inList) {
         List<Integer> outList = copyList(inList);
+        outList.set(1, 12);
+        outList.set(2, 2);
+        return modificationKey(outList);
+    }
+
+    private static List<Integer> modificationKey(List<Integer> outList) {
+        List<Integer> newList = copyList(outList);
         for (int i = 0; i <= outList.size() / 4; i++) {
-            outList.set(1, 12);
-            outList.set(2, 2);
-            int key = outList.get((4 * i));
-            int in1Index = outList.get(1 + (4 * i));
-            int in2Index = outList.get(2 + (4 * i));
-            int outIndex = outList.get(3 + (4 * i));
+            int key = newList.get((4 * i));
+            int in1Index = newList.get(1 + (4 * i));
+            int in2Index = newList.get(2 + (4 * i));
+            int outIndex = newList.get(3 + (4 * i));
             if (key == 1) {
-                int outValue = outList.get(in1Index) + outList.get(in2Index);
-                outList.set(outIndex, outValue);
+                int outValue = newList.get(in1Index) + newList.get(in2Index);
+                newList.set(outIndex, outValue);
             }
             if (key == 2) {
-                int outValue = outList.get(in1Index) * outList.get(in2Index);
-                outList.set(outIndex, outValue);
+                int outValue = newList.get(in1Index) * newList.get(in2Index);
+                newList.set(outIndex, outValue);
             }
             if (key == 99) {
-                return outList;
+                return newList;
             }
         }
-        return outList;
+        return newList;
     }
 
     private static int nextTry(List<Integer> inList) {
 
         List<Integer> copyOfInList = copyList(inList);
-
-
 
 
         int wantedValue = 19690720;
@@ -68,14 +71,14 @@ public class Day2 {
         copyOfInList.set(1, valueNoun);
         copyOfInList.set(2, valueVerb);
 
-        while (valueVerb < 99 && !(modifiedList(copyOfInList)).get(0).equals(wantedValue)) {
+        while (valueVerb < 99 && !(modificationKey(copyOfInList)).get(0).equals(wantedValue)) {
             try {
                 valueVerb++;
                 copyOfInList.set(2, valueVerb);
-                if (!(modifiedList(copyOfInList)).get(0).equals(wantedValue)) {
+                if (!(modificationKey(copyOfInList)).get(0).equals(wantedValue)) {
                     for (valueNoun = 0; valueNoun <= 99; valueNoun++) {
                         copyOfInList.set(1, valueNoun);
-                        if ((modifiedList(copyOfInList)).get(0).equals(wantedValue)) {
+                        if ((modificationKey(copyOfInList)).get(0).equals(wantedValue)) {
                             break;
                         }
                     }
