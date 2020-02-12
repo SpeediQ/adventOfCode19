6fe7b3ba1b3d454a65b7de9524d67cdd80067986
Marcin Kowalczyk <kowalczyk.m@int.pl>
2020-02-12 17:20:52 +0100

    close

--- src/main/java/pl/kowalczyk/day3/Area.java
+++ src/main/java/pl/kowalczyk/day3/Area.java
@@ -8,13 +8,42 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ReadFile {
-    public Point sizeOfMap_DRPoint(List<String> listOfString) {
+public class Area {
+    private int mapV = 0, mapH = 0;
+
+    public int getMapV() {
+        return mapV;
+    }
+
+    public int getMapH() {
+        return mapH;
+    }
+
+    public int getMaxU() {
+        return maxU;
+    }
+
+    public int getMaxD() {
+        return maxD;
+    }
+
+    public int getMaxR() {
+        return maxR;
+    }
+
+    public int getMaxL() {
+        return maxL;
+    }
+
+    private int maxU = 0, maxD = 0, maxR = 0, maxL = 0;
+
+
+
+    boolean[][] AreaSize(List<String> listOfString) {
         String sumOfList = listOfString.get(0) + listOfString.get(1);
         Pattern pattern = Pattern.compile("(\\D)(\\d{1,3})");
         Matcher matcher = pattern.matcher(sumOfList);
-        int mapV=0, mapH = 0;
-        int maxU = 0, maxD = 0, maxR = 0, maxL = 0;
+
         while (matcher.find()) {
 
             if (matcher.group(1).equals("U") && Integer.parseInt(matcher.group(2)) > maxU) {
@@ -29,22 +58,23 @@ public class ReadFile {
             if (matcher.group(1).equals("L") && Integer.parseInt(matcher.group(2)) > maxL) {
                 maxL = Integer.parseInt(matcher.group(2));
             }
-
-
-            mapH = maxL + maxR + 1;
-            mapV = maxU + maxD + 1;
-
         }
+        mapH = maxL + maxR + 1;
+        mapV = maxU + maxD + 1;
         System.out.println("Max U= " + maxU);
         System.out.println("Max D= " + maxD);
         System.out.println("Max R= " + maxR);
         System.out.println("Max L= " + maxL);
-        return new Point(mapH, mapV);
+
+        System.out.println("poziom: "+mapH);
+        System.out.println("pion: "+mapV);
+
+        return new boolean[mapH][mapV];
 
     }
 
 
-    private static List<String> read() throws FileNotFoundException {
+    public List<String> readFromFile() throws FileNotFoundException {
         List<String> WiresList = new ArrayList<>();
         File file = new File("day3.txt");
         Scanner sc = new Scanner(file);
--- src/main/java/pl/kowalczyk/day3/Main.java
+++ src/main/java/pl/kowalczyk/day3/Main.java
@@ -4,8 +4,15 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        ReadFile readFile = new ReadFile();
-        readFile.sizeOfMap_DRPoint(ReadFile.read());
+        Area area = new Area();
+        boolean[][] panel = area.AreaSize(area.readFromFile());
+
+        int h0 = area.getMaxL() + 1;
+        int v0 = area.getMaxU() + 1;
+        Point head = new Point(h0, v0);
+        System.out.println(head);
+
+
     }
 
 }
