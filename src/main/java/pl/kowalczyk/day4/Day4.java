f9263fc666a8765dbba202b76505e2f0fd387a29
Marcin Kowalczyk <kowalczyk.m@int.pl>
2020-02-23 11:18:21 +0100

    day4/1 done day4/2 done
    clear code

--- src/main/java/pl/kowalczyk/day4/Day4.java
+++ src/main/java/pl/kowalczyk/day4/Day4.java
@@ -6,6 +6,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day4 {
+
     public static void main(String[] args) {
         List<String> strings = FileUtil.loadFile("files/day4.txt");
         String[] split = strings.get(0).split("-");
@@ -15,14 +16,13 @@ public class Day4 {
 
         List<String> listOfValues = createListOfValues(minValue, maxValue);
         List<String> correctList = correctList_String(listOfValues);
-        int differentPasswords = correctList.size();
+        List<String> revisedList = part2(correctList);
 
 
-        System.out.println("Answer 4.1: " + differentPasswords);
+        System.out.println("Answer 4.1: " + correctList.size());
+        System.out.println("Answer 4.2: " + revisedList.size());
 
     }
-
-
 
 
     public static List<String> createListOfValues(int min, int max) {
@@ -31,6 +31,16 @@ public class Day4 {
             listOfValues_String.add(String.valueOf(i));
         }
         return listOfValues_String;
+    }
+
+    private static List<String> part2(List<String> listOfValues_String) {
+        List<String> correctList = new ArrayList<>();
+        for (String string : listOfValues_String) {
+            if (partOfALargerArray(string)) {
+                correctList.add(string);
+            }
+        }
+        return correctList;
     }
 
     private static List<String> correctList_String(List<String> listOfValues_String) {
@@ -42,7 +52,6 @@ public class Day4 {
         }
         return correctList;
     }
-
 
 
     private static boolean doesItHaveAPair(String string) {
@@ -57,8 +66,7 @@ public class Day4 {
     }
 
     private static boolean risingValues(String string) {
-        boolean isCorrect = false;
-        isCorrect = true;
+        boolean isCorrect = true;
         for (int i = 1; i < string.length(); i++) {
             if (string.charAt(i - 1) > string.charAt(i)) {
                 isCorrect = false;
@@ -68,7 +76,44 @@ public class Day4 {
         return isCorrect;
     }
 
+    private static boolean partOfALargerArray(String string) {
+        boolean isTrue = false;
 
+        for (int i = 0; i < string.length()-1; i++) {
+            String valueMinus="";
+            String value = String.valueOf(string.charAt(i));
+            String valuePlus = String.valueOf(string.charAt(i+1));
+            String valuePlusPlus="";
+            if (i > 0) {
+                valueMinus = String.valueOf(string.charAt(i-1));
+            }
+            if (i <= 3){
+                valuePlusPlus = String.valueOf(string.charAt(i+2));
+            }
+
+            if (value.equals(valuePlus) && !value.equals(valueMinus) && !value.equals(valuePlusPlus)){
+                isTrue = true;
+            }
+
+//            if (i == 0) {
+//                valueMinus = "";
+//            } else {
+//                valueMinus = String.valueOf(string.charAt(i-1));
+//            }
+//            if (i == 4){
+//                valuePlusPlus = "";
+//            } else {
+//                valuePlusPlus = String.valueOf(string.charAt(i+2));
+//            }
+//
+//            if (value.equals(valuePlus) && !value.equals(valueMinus) && !value.equals(valuePlusPlus)){
+//                isTrue = true;
+//            }
+
+        }
+        return isTrue;
+
+    }
 
 
 }
